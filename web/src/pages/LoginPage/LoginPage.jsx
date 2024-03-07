88656798773a3d70a4863125c48bd1fe5662e32d
Alfredo Vasconcelos de Andrade <alfredo.andrade@ccc.ufcg.edu.br>
2024-03-06 21:44:31 -0300

    feat: classgram logo

--- web/src/pages/LoginPage/LoginPage.jsx
+++ web/src/pages/LoginPage/LoginPage.jsx
@@ -51,9 +51,9 @@ const LoginPage = () => {
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
+            className="mx-auto h-20 w-auto"
+            src="/icon.png"
+            alt="Classgram"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Bem Vindo!
@@ -73,7 +73,7 @@ const LoginPage = () => {
               <div className="mt-2">
                 <TextField
                   name="email"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
                   ref={emailRef}
                   autoComplete="email"
                   validation={{
@@ -104,7 +104,7 @@ const LoginPage = () => {
               <div className="mt-2">
                 <PasswordField
                   name="password"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
                   autoComplete="current-password"
                   validation={{
                     required: {
@@ -119,7 +119,7 @@ const LoginPage = () => {
             </div>
 
             <div>
-              <Submit className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              <Submit className="flex w-full justify-center rounded-md bg-cyan-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600">
                 Login
               </Submit>
             </div>
@@ -129,7 +129,7 @@ const LoginPage = () => {
             Não possui conta?{' '}
             <Link
               to={routes.signup()}
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+              className="font-semibold leading-6 text-cyan-600 hover:text-cyan-500"
             >
               Registre-se!
             </Link>
--- web/src/pages/SignupPage/SignupPage.jsx
+++ web/src/pages/SignupPage/SignupPage.jsx
@@ -56,9 +56,9 @@ const SignupPage = () => {
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
+            className="mx-auto h-20 w-auto"
+            src="/icon.png"
+            alt="Classgram"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Registre-se!
@@ -160,7 +160,7 @@ const SignupPage = () => {
               </div>
               <div className="mt-2">
                 <SelectField
-                  className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                  className="font-semibold leading-6 text-cyan-600 hover:text-cyan-500"
                   name="type"
                   validation={{
                     required: true,
@@ -181,7 +181,7 @@ const SignupPage = () => {
             </div>
 
             <div>
-              <Submit className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              <Submit className="flex w-full justify-center rounded-md bg-cyan-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600">
                 Registrar
               </Submit>
             </div>
@@ -191,7 +191,7 @@ const SignupPage = () => {
             Já possui conta?{' '}
             <Link
               to={routes.login()}
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+              className="font-semibold leading-6 text-cyan-600 hover:text-cyan-500"
             >
               Faça Login!
             </Link>
