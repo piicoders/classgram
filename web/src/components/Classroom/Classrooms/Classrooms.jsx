52b3a4f4281b76c79dbe02c7e15b300a1d63e391
Alfredo Vasconcelos de Andrade <alfredo.andrade@ccc.ufcg.edu.br>
2024-03-08 15:39:11 -0300

    style: styling classroom card

--- web/src/components/Classroom/Classrooms/Classrooms.jsx
+++ web/src/components/Classroom/Classrooms/Classrooms.jsx
@@ -4,17 +4,18 @@ const ClassroomsList = ({ classrooms }) => {
   return (
     <div className="grid grid-cols-3 gap-4 text-center">
       {classrooms.map((classroom) => (
-        <Link
-          key={classroom.id}
-          className="cursor-pointer border p-4"
-          to={routes.classroom({ id: classroom.id })}
-        >
-          <h1 className="text-lg font-bold">{classroom.name}</h1>
-          <p>Class code:</p>
-          <span className="inline-flex items-center rounded-md bg-blue-300 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
-            {classroom.code}
-          </span>
-        </Link>
+        <div key={classroom.id} className="rounded-md bg-slate-400">
+          <Link
+            className="cursor-pointer p-4"
+            to={routes.classroom({ id: classroom.id })}
+          >
+            <h1 className="text-lg font-bold">{classroom.name}</h1>
+            <p>Class code:</p>
+            <span className="inline-flex items-center rounded-md bg-blue-300 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
+              {classroom.code}
+            </span>
+          </Link>
+        </div>
       ))}
     </div>
   )
