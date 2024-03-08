25220c9eeffeebbb4283198942bf4fcf3352f6fc
Alfredo Vasconcelos de Andrade <alfredo.andrade@ccc.ufcg.edu.br>
2024-03-08 20:40:45 -0300

    fix: tab name

--- web/src/pages/Classroom/ClassroomsPage/ClassroomsPage.jsx
+++ web/src/pages/Classroom/ClassroomsPage/ClassroomsPage.jsx
@@ -1,9 +1,16 @@
+import { Metadata } from '@redwoodjs/web'
+
 import { useAuth } from 'src/auth'
 import ClassroomsCell from 'src/components/Classroom/ClassroomsCell'
 
 const ClassroomsPage = () => {
   const { currentUser } = useAuth()
-  return <ClassroomsCell professorId={currentUser.id} />
+  return (
+    <>
+      <Metadata title="Classrooms" />
+      <ClassroomsCell professorId={currentUser.id} />
+    </>
+  )
 }
 
 export default ClassroomsPage
