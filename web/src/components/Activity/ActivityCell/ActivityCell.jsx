28763f796c5084f6a513c81ac787fe14dede3df8
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2024-03-10 12:03:31 -0300

    feat: add activity components frontend
    Co-Authored-By: Paulo Victor <94625698+paulonc@users.noreply.github.com>

---
+++ web/src/components/Activity/ActivityCell/ActivityCell.jsx
@@ -0,0 +1,27 @@
+import Activity from 'src/components/Activity/Activity'
+
+export const QUERY = gql`
+  query FindActivityById($id: Int!) {
+    activity: activity(id: $id) {
+      id
+      description
+      createdAt
+      dueDate
+      maxSize
+      promptId
+      classroomId
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Activity not found</div>
+
+export const Failure = ({ error }) => (
+  <div className="rw-cell-error">{error?.message}</div>
+)
+
+export const Success = ({ activity }) => {
+  return <Activity activity={activity} />
+}
---
+++ web/src/components/Activity/EditActivityCell/EditActivityCell.jsx
@@ -0,0 +1,77 @@
+import { navigate, routes } from '@redwoodjs/router'
+
+import { useMutation } from '@redwoodjs/web'
+import { toast } from '@redwoodjs/web/toast'
+
+import ActivityForm from 'src/components/Activity/ActivityForm'
+
+export const QUERY = gql`
+  query EditActivityById($id: Int!) {
+    activity: activity(id: $id) {
+      id
+      description
+      createdAt
+      dueDate
+      maxSize
+      promptId
+      classroomId
+    }
+  }
+`
+
+const UPDATE_ACTIVITY_MUTATION = gql`
+  mutation UpdateActivityMutation($id: Int!, $input: UpdateActivityInput!) {
+    updateActivity(id: $id, input: $input) {
+      id
+      description
+      createdAt
+      dueDate
+      maxSize
+      promptId
+      classroomId
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Failure = ({ error }) => (
+  <div className="rw-cell-error">{error?.message}</div>
+)
+
+export const Success = ({ activity }) => {
+  const [updateActivity, { loading, error }] = useMutation(
+    UPDATE_ACTIVITY_MUTATION,
+    {
+      onCompleted: () => {
+        toast.success('Activity updated')
+        navigate(routes.activities())
+      },
+      onError: (error) => {
+        toast.error(error.message)
+      },
+    }
+  )
+
+  const onSave = (input, id) => {
+    updateActivity({ variables: { id, input } })
+  }
+
+  return (
+    <div className="rw-segment">
+      <header className="rw-segment-header">
+        <h2 className="rw-heading rw-heading-secondary">
+          Edit Activity {activity?.id}
+        </h2>
+      </header>
+      <div className="rw-segment-main">
+        <ActivityForm
+          activity={activity}
+          onSave={onSave}
+          error={error}
+          loading={loading}
+        />
+      </div>
+    </div>
+  )
+}
