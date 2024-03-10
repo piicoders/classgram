f245fca407124b5ee7b1dc7cb6cf0fa8b8e0095a
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2024-03-10 12:06:02 -0300

    feat: add activity pages frontend
    Co-Authored-By: Paulo Victor <94625698+paulonc@users.noreply.github.com>

---
+++ web/src/pages/Activity/EditActivityPage/EditActivityPage.jsx
@@ -0,0 +1,7 @@
+import EditActivityCell from 'src/components/Activity/EditActivityCell'
+
+const EditActivityPage = ({ id }) => {
+  return <EditActivityCell id={id} />
+}
+
+export default EditActivityPage
