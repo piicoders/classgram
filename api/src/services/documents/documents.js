f038c603f9f0c67fc41344ad55d173c724ca4675
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2024-03-10 12:02:23 -0300

    feat: add document backend
    Co-Authored-By: Paulo Victor <94625698+paulonc@users.noreply.github.com>

---
+++ api/src/services/documents/documents.js
@@ -0,0 +1,42 @@
+import { db } from 'src/lib/db'
+
+export const documents = () => {
+  return db.document.findMany()
+}
+
+export const document = ({ id }) => {
+  return db.document.findUnique({
+    where: { id },
+  })
+}
+
+export const createDocument = ({ input }) => {
+  return db.document.create({
+    data: input,
+  })
+}
+
+export const updateDocument = ({ id, input }) => {
+  return db.document.update({
+    data: input,
+    where: { id },
+  })
+}
+
+export const deleteDocument = ({ id }) => {
+  return db.document.delete({
+    where: { id },
+  })
+}
+
+export const Document = {
+  activity: (_obj, { root }) => {
+    return db.document.findUnique({ where: { id: root?.id } }).activity()
+  },
+  student: (_obj, { root }) => {
+    return db.document.findUnique({ where: { id: root?.id } }).student()
+  },
+  Correction: (_obj, { root }) => {
+    return db.document.findUnique({ where: { id: root?.id } }).Correction()
+  },
+}
