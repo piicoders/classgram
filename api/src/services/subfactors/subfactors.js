dfeb938a6f8710d1556dc0e782ecbce501a0efff
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2024-03-10 12:02:52 -0300

    feat: add subfactor backend
    Co-Authored-By: Paulo Victor <94625698+paulonc@users.noreply.github.com>

---
+++ api/src/services/subfactors/subfactors.js
@@ -0,0 +1,39 @@
+import { db } from 'src/lib/db'
+
+export const subfactors = () => {
+  return db.subfactor.findMany()
+}
+
+export const subfactor = ({ id }) => {
+  return db.subfactor.findUnique({
+    where: { id },
+  })
+}
+
+export const createSubfactor = ({ input }) => {
+  return db.subfactor.create({
+    data: input,
+  })
+}
+
+export const updateSubfactor = ({ id, input }) => {
+  return db.subfactor.update({
+    data: input,
+    where: { id },
+  })
+}
+
+export const deleteSubfactor = ({ id }) => {
+  return db.subfactor.delete({
+    where: { id },
+  })
+}
+
+export const Subfactor = {
+  criterion: (_obj, { root }) => {
+    return db.subfactor.findUnique({ where: { id: root?.id } }).criterion()
+  },
+  Correction: (_obj, { root }) => {
+    return db.subfactor.findUnique({ where: { id: root?.id } }).Correction()
+  },
+}
