8c7c8c3c8bdddb7d50db96dbe59dbb8343e391d8
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2024-03-13 21:38:18 -0300

    feat: add unfinished activities to class
    Co-Authored-By: Alfredo Vasconcelos de Andrade <alfredo.andrade@ccc.ufcg.edu.br>
    Co-Authored-By: Paulo Victor <94625698+paulonc@users.noreply.github.com>
    Co-Authored-By: Bruno Grangeiro Bonifácio <109104329+Bruno-Grangeiro@users.noreply.github.com>

--- web/src/components/Classroom/Classrooms/Classrooms.jsx
+++ web/src/components/Classroom/Classrooms/Classrooms.jsx
@@ -9,16 +9,21 @@ const ClassroomsList = ({ classrooms }) => {
             className="cursor-pointerp-4"
             to={routes.classroom({ id: classroom.id })}
           >
-            <div className="h-16 rounded-md bg-slate-400">
+            <div className="h-16 rounded-md bg-slate-400 text-base truncate">
               <h1 className="text-lg font-bold">{classroom.name}</h1>
               <h1 className="text-lg font-bold">{classroom.professor.name}</h1>
             </div>
           </Link>
           <div className="activities-section h-40 overflow-y-auto rounded bg-slate-200">
-            <h3 className="text-lg font-semibold">Atividades</h3>
             <ul>
-              {classroom.activities?.map((activity) => (
-                <li key={activity.id}>{activity.name}</li>
+              {classroom.Activity?.slice(0, 5).map((activity) => (
+                <Link
+                to={routes.activity({ classId: classroom.id, activityId: activity.id })}
+                >
+                  <li key={activity.id} className="text-base truncate mt-2">
+                    {`${new Date(activity.dueDate).getDate()}/${new Date(activity.dueDate).getMonth() + 1} - ${activity.name} - ${new Date(activity.dueDate).getHours()}:${new Date(activity.dueDate).getMinutes()}h`}
+                  </li>
+                </Link>
               ))}
             </ul>
           </div>
