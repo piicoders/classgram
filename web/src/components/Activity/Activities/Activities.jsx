246c1f4d8565e2c1dab759ac71f5c2495ea4952e
Alfredo Vasconcelos de Andrade <alfredo.andrade@ccc.ufcg.edu.br>
2024-03-16 14:00:55 -0300

    style: change activities listing

--- web/src/components/Activity/Activities/Activities.jsx
+++ web/src/components/Activity/Activities/Activities.jsx
@@ -1,39 +1,6 @@
-import { Link, routes } from '@redwoodjs/router'
-import { useMutation } from '@redwoodjs/web'
-import { toast } from '@redwoodjs/web/toast'
-
-import { QUERY } from 'src/components/Activity/ActivitiesCell'
 import { timeTag, truncate } from 'src/lib/formatters'
 
-const DELETE_ACTIVITY_MUTATION = gql`
-  mutation DeleteActivityMutation($id: Int!) {
-    deleteActivity(id: $id) {
-      id
-    }
-  }
-`
-
 const ActivitiesList = ({ activities }) => {
-  const [deleteActivity] = useMutation(DELETE_ACTIVITY_MUTATION, {
-    onCompleted: () => {
-      toast.success('Activity deleted')
-    },
-    onError: (error) => {
-      toast.error(error.message)
-    },
-    // This refetches the query on the list page. Read more about other ways to
-    // update the cache over here:
-    // https://www.apollographql.com/docs/react/data/mutations/#making-all-other-cache-updates
-    refetchQueries: [{ query: QUERY }],
-    awaitRefetchQueries: true,
-  })
-
-  const onDeleteClick = (id) => {
-    if (confirm('Are you sure you want to delete activity ' + id + '?')) {
-      deleteActivity({ variables: { id } })
-    }
-  }
-
   return (
     <div className="rw-segment rw-table-wrapper-responsive">
       <table className="rw-table">
@@ -44,8 +11,6 @@ const ActivitiesList = ({ activities }) => {
             <th>Created at</th>
             <th>Due date</th>
             <th>Max size</th>
-            <th>Prompt id</th>
-            <th>Classroom id</th>
             <th>&nbsp;</th>
           </tr>
         </thead>
@@ -57,40 +22,6 @@ const ActivitiesList = ({ activities }) => {
               <td>{timeTag(activity.createdAt)}</td>
               <td>{timeTag(activity.dueDate)}</td>
               <td>{truncate(activity.maxSize)}</td>
-              <td>{truncate(activity.promptId)}</td>
-              <td>{truncate(activity.classroomId)}</td>
-              <td>
-                <nav className="rw-table-actions">
-                  <Link
-                    to={routes.activity({
-                      activityId: activity.id,
-                      classId: activity.classroomId,
-                    })}
-                    title={'Show activity ' + activity.id + ' detail'}
-                    className="rw-button rw-button-small"
-                  >
-                    Show
-                  </Link>
-                  <Link
-                    to={routes.editActivity({
-                      activityId: activity.id,
-                      classId: activity.classroomId,
-                    })}
-                    title={'Edit activity ' + activity.id}
-                    className="rw-button rw-button-small rw-button-blue"
-                  >
-                    Edit
-                  </Link>
-                  <button
-                    type="button"
-                    title={'Delete activity ' + activity.id}
-                    className="rw-button rw-button-small rw-button-red"
-                    onClick={() => onDeleteClick(activity.id)}
-                  >
-                    Delete
-                  </button>
-                </nav>
-              </td>
             </tr>
           ))}
         </tbody>
