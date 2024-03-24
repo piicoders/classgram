22e99f99273c3ed2d592eb3b76fdafaeba5882f6
Alfredo Vasconcelos de Andrade <alfredo.andrade@ccc.ufcg.edu.br>
2024-03-24 19:51:55 -0300

    style: styling activity screen

--- web/src/components/Activity/Activity/Activity.jsx
+++ web/src/components/Activity/Activity/Activity.jsx
@@ -52,10 +52,26 @@ const Activity = ({ activity }) => {
   return (
     <div className="mx-auto mt-8 max-w-6xl px-8">
       <div className="overflow-hidden rounded-lg bg-white shadow-lg">
-        <div className="flex items-start justify-between px-10 py-0">
-          <h2 className="mb-6 text-3xl font-semibold text-gray-800">
-            {activity.name}
-          </h2>
+        <div className="flex items-center justify-between px-10 py-0">
+          <div className="flex flex-col">
+            <h2 className="mb-2 text-3xl font-semibold text-gray-800">
+              {activity.name}
+            </h2>
+            <p
+              className={`mb-6 text-lg ${
+                new Date(activity.dueDate) > new Date()
+                  ? 'text-gray-600'
+                  : 'text-red-600'
+              }`}
+            >
+              Prazo:{' '}
+              {`${new Date(activity.dueDate).getDate()}/${
+                new Date(activity.dueDate).getMonth() + 1
+              } - ${new Date(activity.dueDate).getHours()}:${new Date(
+                activity.dueDate
+              ).getMinutes()}h`}
+            </p>
+          </div>
           {currentUser.type == 'P' ? (
             <div className="flex items-center">
               <Link
@@ -65,14 +81,14 @@ const Activity = ({ activity }) => {
                 })}
                 className="mr-4 text-blue-500 hover:text-blue-700"
               >
-                Edit
+                Editar
               </Link>
               <button
                 type="button"
                 className="text-red-500 hover:text-red-700"
                 onClick={() => onDeleteClick(activity.id)}
               >
-                Delete
+                Apagar
               </button>
             </div>
           ) : (
@@ -80,18 +96,10 @@ const Activity = ({ activity }) => {
           )}
         </div>
         <div className="border-t border-gray-200 bg-gray-100 px-8 py-4"></div>
-        <div className="px-10 py-8">
-          <p className="mb-4 text-lg text-gray-600">
-            Created at: {timeTag(activity.createdAt)}
-          </p>
-          <p className="mb-4 text-lg text-gray-600">
-            Due date: {timeTag(activity.dueDate)}
-          </p>
-          <p className="mb-4 text-lg text-gray-600">
-            Description: {activity.description}
-          </p>
+        <div className="px-8 py-4">
+          <p className="mb-4 text-lg text-gray-600">{activity.description}</p>
           <textarea
-            className="h-96 w-full rounded-lg border px-3 py-2 text-base focus:border-blue-300 focus:outline-none focus:ring"
+            className="h-48 w-full rounded-lg border px-3 py-2 text-base focus:border-blue-300 focus:outline-none focus:ring"
             value={response}
             onChange={handleChange}
             maxLength={activity.maxSize}
