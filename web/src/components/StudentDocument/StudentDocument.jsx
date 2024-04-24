95c4bb3ad073ca3f448ddfcfd14513cfacac49cf
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2024-04-24 19:00:14 -0300

    style: update comments style

--- web/src/components/StudentDocument/StudentDocument.jsx
+++ web/src/components/StudentDocument/StudentDocument.jsx
@@ -266,14 +266,14 @@ const StudentDocument = ({ document, title, corrections }) => {
       ) : (
         ''
       )}
-      {comments ? (
+      {comments.length ? (
         <>
           <hr className="my-16 border-gray-300" />
           <h2 className="mb-4 text-3xl font-bold text-gray-900">
             <span className="border-l-4 border-blue-500 pl-2">Comentários</span>
           </h2>
           {comments.map((comment) => (
-            <div key={comment.id} className="mb-4">
+            <div key={comment.id} className="mb-4 rounded-lg bg-white p-6 shadow-md">
               <h1 className='mb-4 text-2xl font-bold text-gray-900'>{comment.user.name}:</h1>
               <p>{comment.content}</p>
             </div>
