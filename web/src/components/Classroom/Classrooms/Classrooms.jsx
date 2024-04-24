c13fa884f781a54d1ea50401077f291bdcbbfc55
paulonc <paulo.cavalcanti@ccc.ufcg.edu.br>
2024-04-24 20:30:59 -0300

    refactor: Improve class viewing layout

--- web/src/components/Classroom/Classrooms/Classrooms.jsx
+++ web/src/components/Classroom/Classrooms/Classrooms.jsx
@@ -1,37 +1,50 @@
 import { Link, routes } from '@redwoodjs/router'
 
+import { useAuth } from 'src/auth'
+
 const ClassroomsList = ({ classrooms }) => {
+  const { currentUser } = useAuth()
+
   return (
-    <div className="grid grid-cols-4 gap-4 text-center">
+    <div className="grid grid-cols-4 gap-4">
       {classrooms.map((classroom) => (
-        <div key={classroom.id} className="rounded border">
-          <Link
-            className="cursor-pointerp-4"
-            to={routes.activities({ classId: classroom.id })}
-          >
-            <div className="h-16 truncate rounded-md bg-slate-400 text-base">
-              <h1 className="text-lg font-bold">{classroom.name}</h1>
-              <h1 className="text-lg font-bold">{classroom.professor.name}</h1>
-            </div>
-          </Link>
-          <div className="activities-section h-40 overflow-y-auto rounded bg-slate-200">
-            <ul>
+        <div key={classroom.id} className="overflow-hidden rounded-md border">
+          <div className="flex h-40 flex-col justify-center bg-gray-200 p-4">
+            <Link to={routes.activities({ classId: classroom.id })}>
+              <h1 className="mb-2 text-center text-lg font-bold text-gray-800">
+                {classroom.name}
+              </h1>
+              {currentUser?.roles == 'S' && (
+                <p className="mb-4 truncate text-center text-gray-600">
+                  Professor: {classroom.professor.name}
+                </p>
+              )}
+            </Link>
+          </div>
+          <div className="bg-white p-4">
+            <ul className="m-0 list-none p-0">
               {classroom.Activity?.slice(0, 5).map((activity) => (
-                <Link
-                  key={activity.id}
-                  to={routes.activity({
-                    classId: classroom.id,
-                    activityId: activity.id,
-                  })}
-                >
-                  <li key={activity.id} className="mt-2 truncate text-base">
-                    {`${new Date(activity.dueDate).getDate()}/${
-                      new Date(activity.dueDate).getMonth() + 1
-                    } - ${activity.name} - ${new Date(
-                      activity.dueDate
-                    ).getHours()}:${new Date(activity.dueDate).getMinutes()}h`}
-                  </li>
-                </Link>
+                <li key={activity.id} className="mb-2 truncate">
+                  <Link
+                    to={routes.activity({
+                      classId: classroom.id,
+                      activityId: activity.id,
+                    })}
+                  >
+                    <span className="mr-2 text-gray-700">
+                      {new Date(activity.dueDate).toLocaleDateString()} -
+                    </span>
+                    <span className="mr-2 text-gray-700">
+                      {activity.name} -
+                    </span>
+                    <span className="text-gray-700">
+                      {new Date(activity.dueDate).toLocaleTimeString([], {
+                        hour: '2-digit',
+                        minute: '2-digit',
+                      })}
+                    </span>
+                  </Link>
+                </li>
               ))}
             </ul>
           </div>
