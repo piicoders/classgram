e2e923177e2b83cdc72bf16c254616ed002fa105
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2024-07-06 15:15:13 -0300

    feat: insert side bar menu on document page

--- web/src/components/StudentDocument/StudentDocument.jsx
+++ web/src/components/StudentDocument/StudentDocument.jsx
@@ -1,5 +1,6 @@
-import { useQuery } from '@redwoodjs/web'
 import React, { useEffect, useState } from 'react'
+
+import { useQuery } from '@redwoodjs/web'
 
 import { useAuth } from 'src/auth'
 import ActivityReview from 'src/components/ActivityReview'
@@ -63,6 +64,41 @@ const getSeverityText = (severity) => {
   return severityMap[severity] || ''
 }
 
+const SidebarMenu = () => {
+  return (
+    <div className="fixed left-4 top-1/4 z-10 w-32 rounded-lg bg-gray-200 p-2 shadow-md">
+      <h2 className="mb-2 text-lg font-bold">Menu</h2>
+      <ul className="space-y-1 text-sm">
+        <li>
+          <a href="#activity" className="text-blue-500 hover:underline">
+            Atividade
+          </a>
+        </li>
+        <li>
+          <a href="#documentContent" className="text-blue-500 hover:underline">
+            Entrega
+          </a>
+        </li>
+        <li>
+          <a href="#grade" className="text-blue-500 hover:underline">
+            Nota
+          </a>
+        </li>
+        <li>
+          <a href="#corrections" className="text-blue-500 hover:underline">
+            Correções
+          </a>
+        </li>
+        <li>
+          <a href="#comments" className="text-blue-500 hover:underline">
+            Comentários
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
 const StudentDocument = ({ document, title, corrections }) => {
   const { currentUser } = useAuth()
 
@@ -82,7 +118,6 @@ const StudentDocument = ({ document, title, corrections }) => {
       setComments(data.commentByDocumentId)
     }
   }, [loading, error, data])
-
 
   const handleModalOpen = () => {
     setShowModal(true)
@@ -105,11 +140,13 @@ const StudentDocument = ({ document, title, corrections }) => {
           <p>
             <strong>Descrição:</strong> {description}
           </p>
-          {correction != null && correction != "null" && correction !== undefined && (
-            <p>
-              <strong>Correção: </strong> {correction}
-            </p>
-          )}
+          {correction != null &&
+            correction != 'null' &&
+            correction !== undefined && (
+              <p>
+                <strong>Correção: </strong> {correction}
+              </p>
+            )}
         </div>
       )
       setPopupPosition(position)
@@ -172,118 +209,132 @@ const StudentDocument = ({ document, title, corrections }) => {
   )
 
   return (
-    <>
-      {showModal && (
-        <ActivityReview documentId={document.id} onClose={handleModalClose} />
-      )}
-      {showPopup && (
-        <MousePopup
-          content={popupContent}
-          position={popupPosition}
-          severity={severity}
-        />
-      )}
-      <div className="mb-16">
-        <h3 className="mb-2 flex items-center justify-between text-2xl font-semibold text-gray-800">
-          {title}
-          {currentUser.roles === 'P' && (
-            <button
-              onClick={handleModalOpen}
-              className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
-            >
-              Avaliar
-            </button>
-          )}
-        </h3>
-        <p className="mb-2 text-sm text-gray-600">
-          {formatDate(document.handed)}
-        </p>
-        <p
-          id="documentContent"
-          className="text-base"
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-          dangerouslySetInnerHTML={{ __html: highlightedContent }}
-        ></p>
-      </div>
-      {document.mark != null ? (
-        <>
-          <hr className="my-16 border-gray-300" />
-          <div className="mb-4 text-gray-900">
-            <h2 className="mb-2 text-3xl">
-              <span className="border-l-4 border-blue-500 pl-2 font-bold">
-                Nota: {document.mark}
+    <div className="relative">
+      {/* Menu lateral flutuante */}
+      <SidebarMenu />
+
+      {/* Conteúdo principal */}
+      <div className="ml-16 p-4">
+        {showModal && (
+          <ActivityReview documentId={document.id} onClose={handleModalClose} />
+        )}
+        {showPopup && (
+          <MousePopup
+            content={popupContent}
+            position={popupPosition}
+            severity={severity}
+          />
+        )}
+        <div className="mb-16">
+          <h3
+            id="documentContent"
+            className="mb-2 flex items-center justify-between text-2xl font-semibold text-gray-800"
+          >
+            {title}
+            {currentUser.roles === 'P' && (
+              <button
+                onClick={handleModalOpen}
+                className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+              >
+                Avaliar
+              </button>
+            )}
+          </h3>
+          <p className="mb-2 text-sm text-gray-600">
+            {formatDate(document.handed)}
+          </p>
+          <p
+            className="text-base"
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOut}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            dangerouslySetInnerHTML={{ __html: highlightedContent }}
+          ></p>
+        </div>
+        {document.mark != null ? (
+          <>
+            <hr id="grade" className="my-16 border-gray-300" />
+            <div className="mb-4 text-gray-900">
+              <h2 className="mb-2 text-3xl">
+                <span className="border-l-4 border-blue-500 pl-2 font-bold">
+                  Nota: {document.mark}
+                </span>
+              </h2>
+              <ul className="list-disc pl-8">
+                {Object.entries(JSON.parse(document.subFactorsMark)).map(
+                  ([key, value]) => (
+                    <li key={key} className="mb-2">
+                      <span className="font-bold">{key}:</span> {value}
+                    </li>
+                  )
+                )}
+              </ul>
+            </div>
+          </>
+        ) : (
+          ''
+        )}
+        {corrections.length ? (
+          <>
+            <hr id="corrections" className="my-16 border-gray-300" />
+            <h2 className="mb-4 text-3xl font-bold text-gray-900">
+              <span className="border-l-4 border-blue-500 pl-2">Correções</span>
+            </h2>
+
+            {corrections.map((correction) => (
+              <div
+                key={correction.id}
+                className="mb-4 rounded-lg bg-white p-6 shadow-md"
+              >
+                <p>
+                  <span className="font-bold">Trecho:</span> {correction.text}
+                </p>
+                <p>
+                  <span className="font-bold">Descrição:</span>{' '}
+                  {correction.description}
+                </p>
+                {correction.correct && (
+                  <p>
+                    <span className="font-bold">Correção:</span>{' '}
+                    {correction.correct}
+                  </p>
+                )}
+                <p>
+                  <span className="font-bold">Severidade:</span>{' '}
+                  {getSeverityText(correction.severity)}
+                </p>
+              </div>
+            ))}
+          </>
+        ) : (
+          ''
+        )}
+        {comments.length ? (
+          <>
+            <hr id="comments" className="my-16 border-gray-300" />
+            <h2 className="mb-4 text-3xl font-bold text-gray-900">
+              <span className="border-l-4 border-blue-500 pl-2">
+                Comentários
               </span>
             </h2>
-            <ul className="list-disc pl-8">
-              {Object.entries(JSON.parse(document.subFactorsMark)).map(
-                ([key, value]) => (
-                  <li key={key} className="mb-2">
-                    <span className="font-bold">{key}:</span> {value}
-                  </li>
-                )
-              )}
-            </ul>
-          </div>
-        </>
-      ) : (
-        ''
-      )}
-      {corrections.length ? (
-        <>
-          <hr className="my-16 border-gray-300" />
-          <h2 className="mb-4 text-3xl font-bold text-gray-900">
-            <span className="border-l-4 border-blue-500 pl-2">Correção</span>
-          </h2>
-
-          {corrections.map((correction) => (
-            <div
-              key={correction.id}
-              className="mb-4 rounded-lg bg-white p-6 shadow-md"
-            >
-              <p>
-                <span className="font-bold">Trecho:</span> {correction.text}
-              </p>
-              <p>
-                <span className="font-bold">Descrição:</span>{' '}
-                {correction.description}
-              </p>
-              {correction.correct && (
-                <p>
-                  <span className="font-bold">Correção:</span>{' '}
-                  {correction.correct}
-                </p>
-              )}
-              <p>
-                <span className="font-bold">Severidade:</span>{' '}
-                {getSeverityText(correction.severity)}
-              </p>
-            </div>
-          ))}
-        </>
-      ) : (
-        ''
-      )}
-      {comments.length ? (
-        <>
-          <hr className="my-16 border-gray-300" />
-          <h2 className="mb-4 text-3xl font-bold text-gray-900">
-            <span className="border-l-4 border-blue-500 pl-2">Comentários</span>
-          </h2>
-          {comments.map((comment) => (
-            <div key={comment.id} className="mb-4 rounded-lg bg-white p-6 shadow-md">
-              <h1 className='mb-4 text-2xl font-bold text-gray-900'>{comment.user.name}:</h1>
-              <p>{comment.content}</p>
-            </div>
-          ))}
-        </>
-      ) : (
-        ''
-      )}
-
-    </>
+            {comments.map((comment) => (
+              <div
+                key={comment.id}
+                className="mb-4 rounded-lg bg-white p-6 shadow-md"
+              >
+                <h1 className="mb-4 text-2xl font-bold text-gray-900">
+                  {comment.user.name}:
+                </h1>
+                <p>{comment.content}</p>
+              </div>
+            ))}
+          </>
+        ) : (
+          ''
+        )}
+      </div>
+    </div>
   )
 }
 
