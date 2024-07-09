f7fac7b5adb31a1234b253aa6147e608ac42b121
paulonc <paulo.cavalcanti@ccc.ufcg.edu.br>
2024-07-09 20:19:43 -0300

    refactor: improve DocumentsList

--- web/src/components/Document/Documents/Documents.jsx
+++ web/src/components/Document/Documents/Documents.jsx
@@ -1,74 +1,41 @@
-import { Link, routes, useParams } from '@redwoodjs/router'
+import { format } from 'date-fns'
+import { FaEye } from 'react-icons/fa'
 
-import { truncate } from 'src/lib/formatters'
+import { Link, routes, useParams } from '@redwoodjs/router'
 
 const DocumentsList = ({ documents }) => {
   const { classId } = useParams()
 
   return (
-    <div className="overflow-x-auto">
-      <table className="whitespace-no-wrap w-full divide-y divide-gray-200 bg-white">
-        <thead className="bg-gray-50">
-          <tr>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
-            >
-              Resposta
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
-            >
-              Data de Entrega
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
-            >
-              Aluno
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
-            >
-              &nbsp;
-            </th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-200">
-          {documents.map((document) => (
-            <tr key={document.id}>
-              <td className="whitespace-normal px-6 py-4">
-                {truncate(document.content)}
-              </td>
-              <td className="whitespace-normal px-6 py-4">
-                {`${new Date(document.handed).getDate()}/${
-                  new Date(document.handed).getMonth() + 1
-                } - ${new Date(document.handed).getHours()}h`}
-              </td>
-              <td className="whitespace-normal px-6 py-4">
-                {truncate(document.student.name)}
-              </td>
-              <td className="whitespace-normal px-6 py-4">
-                <div className="flex items-center space-x-2">
-                  <Link
-                    to={routes.document({
-                      id: document.id,
-                      activityId: document.activityId,
-                      classId: classId,
-                    })}
-                    title={'Ver entrega detalhada'}
-                    className="text-indigo-600 hover:text-indigo-900"
-                  >
-                    Visualizar
-                  </Link>
-                </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="divide-y divide-gray-200">
+      {documents.map((document) => (
+        <div
+          key={document.id}
+          className="py-4 transition duration-300 ease-in-out hover:bg-gray-50"
+        >
+          <div className="flex items-center justify-between">
+            <div>
+              <h3 className="text-lg font-semibold">{document.student.name}</h3>
+              <p className="text-sm text-gray-600">
+                {format(new Date(document.handed), 'dd/MM/yyyy - HH:mm')}
+              </p>
+            </div>
+            <div className="ml-4">
+              <Link
+                to={routes.document({
+                  id: document.id,
+                  activityId: document.activityId,
+                  classId: classId,
+                })}
+                title={'Ver entrega detalhada'}
+                className="flex items-center text-indigo-600 hover:text-indigo-900"
+              >
+                <FaEye className="mr-1" /> Visualizar
+              </Link>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
