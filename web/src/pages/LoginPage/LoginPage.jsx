afbf17d87e648e87b5fc26bb1d2b86b22dd45e14
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2024-07-15 21:38:30 -0300

    feat: add loading to login button

--- web/src/pages/LoginPage/LoginPage.jsx
+++ web/src/pages/LoginPage/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { useEffect } from 'react'
 
 import {
@@ -17,6 +17,7 @@ import { useAuth } from 'src/auth'
 
 const LoginPage = () => {
   const { isAuthenticated, logIn } = useAuth()
+  const [loading, setLoading] = useState(false) // Estado para controlar o carregamento do login
 
   useEffect(() => {
     if (isAuthenticated) {
@@ -30,10 +31,14 @@ const LoginPage = () => {
   }, [])
 
   const onSubmit = async (data) => {
+    setLoading(true) // Inicia o estado de carregamento
+
     const response = await logIn({
       username: data.email,
       password: data.password,
     })
+
+    setLoading(false) // Finaliza o estado de carregamento após a resposta
 
     if (response.message) {
       toast(response.message)
@@ -119,8 +124,39 @@ const LoginPage = () => {
             </div>
 
             <div>
-              <Submit className="flex w-full justify-center rounded-md bg-cyan-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600">
-                Login
+              <Submit
+                className={`flex w-full justify-center rounded-md bg-cyan-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600 ${
+                  loading ? 'cursor-not-allowed opacity-50' : ''
+                }`}
+                disabled={loading}
+              >
+                {loading ? (
+                  <>
+                    <svg
+                      className="-ml-1 mr-3 h-5 w-5 animate-spin text-white"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                      ></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
+                    </svg>
+                    Carregando...
+                  </>
+                ) : (
+                  'Login'
+                )}
               </Submit>
             </div>
           </Form>
