114bad6944aa8e032da3af49c38732bec28920ce
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2024-07-15 21:25:02 -0300

    feat: add classroom header

--- web/src/pages/Activity/ActivitiesPage/ActivitiesPage.jsx
+++ web/src/pages/Activity/ActivitiesPage/ActivitiesPage.jsx
@@ -1,49 +1,15 @@
 import { useParams } from '@redwoodjs/router'
-import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
-import { useAuth } from 'src/auth'
 import ActivitiesCell from 'src/components/Activity/ActivitiesCell'
+import ClassroomPage from 'src/pages/Classroom/ClassroomPage/ClassroomPage'
 
 const ActivitiesPage = () => {
   const { classId } = useParams()
-  const { currentUser } = useAuth()
   return (
     <>
       <Metadata title="Atividades" />
-      <div className="mb-5 flex items-center justify-between rounded-lg border border-gray-300 px-6 py-4">
-        <div className="flex gap-4">
-          <Link
-            className={
-              'text-cyan-600 underline transition duration-300 hover:text-gray-900'
-            }
-          >
-            Atividades
-          </Link>
-          {currentUser.roles == 'P' ? (
-            <Link
-              className={
-                'text-gray-700 transition duration-300 hover:text-gray-900 '
-              }
-              to={routes.users({ classId: classId })}
-            >
-              Alunos
-            </Link>
-          ) : (
-            ''
-          )}
-        </div>
-        {currentUser.roles == 'P' ? (
-          <Link
-            to={routes.newActivity({ classId: classId })}
-            className="rw-button rw-button-green"
-          >
-            <div className="rw-button-icon">+</div> Nova Atividade
-          </Link>
-        ) : (
-          ''
-        )}
-      </div>
+      <ClassroomPage id={classId} />
       <ActivitiesCell classId={classId} />
     </>
   )
